8afeb4f4344a4496ff6f5de6b051df2f5d00dc23
artlvns <artjoms.laivins@hotmail.com>
2024-11-18 10:07:32 +0100

    feat!: support azurerm v4

--- providers.tf
+++ providers.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.6.2"
+      version = "~> 3"
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.113.0"
+      version = "~> 4"
     }
   }
 
